dc4805a0a5952f0b577c3ddd07a972d6d5d278f7
colehattler <cwh31@duke.edu>
2020-02-26 15:28:36 -0500

    Added multiply functionality to noop

--- code.py
+++ code.py
@@ -1,9 +1,10 @@
 def add(a,b):
     return a+b
 
-def no_op():
+def no_op(mult1 = 1, mult2 = 2):
     '''
         Write a meaningful function here, unlike the one above
 
         Then, add a test case for this function in tests/test.py'''
-    return
+    
+    return mult1 * mult2
