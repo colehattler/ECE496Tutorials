5c3e2e685435fb5d8122ae48b1c532eec6a4dea4
colehattler <cwh31@duke.edu>
2020-02-26 15:30:24 -0500

    Created test for noop

--- tests/test.py
+++ tests/test.py
@@ -11,6 +11,10 @@ class TestCases(unittest.TestCase):
 	Add your own test cases here
 	function name must start with test_
 	'''
-
+	
+	def test_no_op(self):
+		self.asserEqual(2, no_op())
+		self.assertEqual(9, no_op(3,3))
+		
 if __name__ == "__main__":
 	unittest.main()
